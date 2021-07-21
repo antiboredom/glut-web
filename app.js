ab9aff0ae310737a00df55b5702fa71fc3d5673c
Sam Lavigne <splavigne@gmail.com>
2021-07-21 16:30:24 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+// const buildUrl = "https://www.hotknife.co.uk/Glut/Build";
+// const loaderUrl = buildUrl + "/WebBuild.loader.js";
+// const config = {
+//   dataUrl: buildUrl + "/WebBuild.data",
+//   frameworkUrl: buildUrl + "/WebBuild.framework.js",
+//   codeUrl: buildUrl + "/WebBuild.wasm",
+//   streamingAssetsUrl: "StreamingAssets",
+//   companyName: "DefaultCompany",
+//   productName: "Glut_2021",
+//   productVersion: "0.1",
+// };
+//
+// const canvas = document.querySelector("#game");
+//
+// createUnityInstance(canvas, config, (progress) => {
+//   console.log('progress');
+//   // progressBarFull.style.width = 100 * progress + "%";
+// })
+//   .then((unityInstance) => {
+//     // loadingBar.style.display = "none";
+//     // fullscreenButton.onclick = () => {
+//     //   unityInstance.SetFullscreen(1);
+//     // };
+//   })
+//   .catch((message) => {
+//     alert(message);
+//   });
